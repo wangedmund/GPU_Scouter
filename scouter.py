bd4e51e8b0e87d6bddc9d9d7544f8dd3bd79c358
Edmund Wang <wangedmund@berkeley.edu>
2021-01-09 03:00:55 -0800

    forgot to change back to heroku path

--- scouter.py
+++ scouter.py
@@ -29,8 +29,8 @@ class Scouter():
         chrome_options.add_argument("--headless")
         chrome_options.add_argument("--disable-dev-shm-usage")
         chrome_options.add_argument("--no-sandbox")
-        # driver = webdriver.Chrome(executable_path=os.environ.get("CHROMEDRIVER_PATH"), chrome_options=chrome_options)
-        driver = webdriver.Chrome(LOCAL_PATH)
+        driver = webdriver.Chrome(executable_path=os.environ.get("CHROMEDRIVER_PATH"), chrome_options=chrome_options)
+        # driver = webdriver.Chrome(LOCAL_PATH)
         driver.get(self.link)
         while True:
             try:
