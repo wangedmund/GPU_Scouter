a6226d9d82978d06bf901a422941e475c0474f7d
Edmund Wang <wangedmund@berkeley.edu>
2021-01-09 02:01:36 -0800

    pls work

--- scouter.py
+++ scouter.py
@@ -5,8 +5,6 @@ from selenium.webdriver.common.by import By
 import os 
 
 WEBHOOK = "https://discord.com/api/webhooks/758151139383443478/XURX5jr3xmCPN6OPoTV2bGP-YLhD8K_c4roR1_KivVZZ8TnPfOuuQqQVyrJdxQP1DV2A"
-PATH = "/app/.chromedriver/bin/chromedriver"
-print(PATH)
 RTX3070LINK = "https://www.bestbuy.com/site/nvidia-geforce-rtx-3070-8gb-gddr6-pci-express-4-0-graphics-card-dark-platinum-and-black/6429442.p?skuId=6429442"
 RTX3080LINK = "https://www.bestbuy.com/site/nvidia-geforce-rtx-3080-10gb-gddr6x-pci-express-4-0-graphics-card-titanium-and-black/6429440.p?skuId=6429440"
 
@@ -19,7 +17,12 @@ class Scouter():
     }
         requests.post(WEBHOOK, json=d)
     def scout(self):
-        driver = webdriver.Chrome(PATH)
+        chrome_options = webdriver.ChromeOptions()
+        chrome_options.binary_location = os.environ.get("GOOGLE_CHROME_BIN")
+        chrome_options.add_argument("--headless")
+        chrome_options.add_argument("--disable-dev-shm-usage")
+        chrome_options.add_argument("--no-sandbox")
+        driver = webdriver.Chrome(executable_path=os.environ.get("CHROMEDRIVER_PATH"), chrome_options=chrome_options)
         driver.get(self.link)
         while True:
             try:
