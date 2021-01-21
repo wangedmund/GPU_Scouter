87bdf10a818a5edfa12454a3ee8bd28995b89ca9
Edmund Wang <wangedmund@berkeley.edu>
2021-01-20 21:31:33 -0800

    switched out 3080 for 5800x

--- scouter.py
+++ scouter.py
@@ -8,6 +8,7 @@ from multiprocessing import Process
 WEBHOOK = "https://discord.com/api/webhooks/758151139383443478/XURX5jr3xmCPN6OPoTV2bGP-YLhD8K_c4roR1_KivVZZ8TnPfOuuQqQVyrJdxQP1DV2A"
 RTX3070LINK = "https://www.bestbuy.com/site/nvidia-geforce-rtx-3070-8gb-gddr6-pci-express-4-0-graphics-card-dark-platinum-and-black/6429442.p?skuId=6429442"
 RTX3080LINK = "https://www.bestbuy.com/site/nvidia-geforce-rtx-3080-10gb-gddr6x-pci-express-4-0-graphics-card-titanium-and-black/6429440.p?skuId=6429440"
+RYZEN7LINK = "https://www.bestbuy.com/site/amd-ryzen-7-5800x-4th-gen-8-core-16-threads-unlocked-desktop-processor-without-cooler/6439000.p?skuId=6439000"
 HYPERXBLACK = "https://www.bestbuy.com/site/hyperx-cloud-alpha-pro-wired-stereo-gaming-headset-for-pc-ps4-xbox-one-blackout-black/6434292.p?skuId=6434292"
 HYPERXRED = "https://www.bestbuy.com/site/hyperx-cloud-alpha-wired-stereo-gaming-headset-for-pc-ps4-xbox-one-and-nintendo-switch-red-black/6100109.p?skuId=6100109"
 
@@ -44,7 +45,7 @@ class Scouter():
             
 if __name__=='__main__':
     scouter1 = Scouter(RTX3070LINK, "3070")
-    scouter2 = Scouter(RTX3080LINK, "3080")
+    scouter2 = Scouter(RYZEN7LINK, "5800x")
     p1 = Process(target = scouter1.scout)
     p1.start()
     p2 = Process(target = scouter2.scout)
