cead660f1bb63a2ddbb7f7baf15b4579a817f8d5
Edmund Wang <wangedmund@berkeley.edu>
2021-01-24 16:55:34 -0800

    fixed scouter

--- scouter.py
+++ scouter.py
@@ -4,25 +4,27 @@ from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
 import os 
 from multiprocessing import Process
+from webdriver_manager.chrome import ChromeDriverManager
+import time
 
 WEBHOOK = "https://discord.com/api/webhooks/758151139383443478/XURX5jr3xmCPN6OPoTV2bGP-YLhD8K_c4roR1_KivVZZ8TnPfOuuQqQVyrJdxQP1DV2A"
 RTX3070LINK = "https://www.bestbuy.com/site/nvidia-geforce-rtx-3070-8gb-gddr6-pci-express-4-0-graphics-card-dark-platinum-and-black/6429442.p?skuId=6429442"
+RTX3070XPATH = '//button[@data-sku-id = "6429442"]'
 RTX3080LINK = "https://www.bestbuy.com/site/nvidia-geforce-rtx-3080-10gb-gddr6x-pci-express-4-0-graphics-card-titanium-and-black/6429440.p?skuId=6429440"
 RYZEN7LINK = "https://www.bestbuy.com/site/amd-ryzen-7-5800x-4th-gen-8-core-16-threads-unlocked-desktop-processor-without-cooler/6439000.p?skuId=6439000"
-HYPERXBLACK = "https://www.bestbuy.com/site/hyperx-cloud-alpha-pro-wired-stereo-gaming-headset-for-pc-ps4-xbox-one-blackout-black/6434292.p?skuId=6434292"
-HYPERXRED = "https://www.bestbuy.com/site/hyperx-cloud-alpha-wired-stereo-gaming-headset-for-pc-ps4-xbox-one-and-nintendo-switch-red-black/6100109.p?skuId=6100109"
+RYZEN7XPATH = '//button[@data-sku-id = "6439000"]'
 
 LOCAL_PATH = os.getcwd() + "\chromedriver.exe"
 
 class Scouter():
-    def __init__(self, link, name):
+    def __init__(self, link, name, XPATH):
         self.link = link
         self.name = name
+        self.XPATH = XPATH
     def sendToDiscord(self, link):
-        #<@&691835932835577856>
         d = {
         "content": "<@&691835932835577856>\nBuy Now:\n" + link
-    }
+        }
         requests.post(WEBHOOK, json=d)
     def scout(self):
         chrome_options = webdriver.ChromeOptions()
@@ -30,25 +32,34 @@ class Scouter():
         chrome_options.add_argument("--headless")
         chrome_options.add_argument("--disable-dev-shm-usage")
         chrome_options.add_argument("--no-sandbox")
-        driver = webdriver.Chrome(executable_path=os.environ.get("CHROMEDRIVER_PATH"), chrome_options=chrome_options)
+        # driver = webdriver.Chrome(executable_path=os.environ.get("CHROMEDRIVER_PATH"), chrome_options=chrome_options)
+        driver = webdriver.Chrome(ChromeDriverManager(version="87.0.4280.88").install())
         # driver = webdriver.Chrome(LOCAL_PATH)
         driver.get(self.link)
         while True:
             try:
-                elem = driver.find_element(By.XPATH, '//button[text()="Add to Cart"]')
+                elem = driver.find_element(By.XPATH, self.XPATH)
             except:
-                print(self.name + " could not be found")
+                print(self.name + "element could not be found!")
                 driver.refresh()
                 continue
-            print(self.name + " add to cart button found")
-            self.sendToDiscord(self.link)
+            if (elem.text != 'Sold Out'):
+                print(self.name + "'s " +  elem.text + " button found!")
+                self.sendToDiscord(self.link)
+                time.sleep(300)
+            else:
+                print(self.name + " sold out!")
+            driver.refresh()
             
 if __name__=='__main__':
-    scouter1 = Scouter(RTX3070LINK, "3070")
-    scouter2 = Scouter(RYZEN7LINK, "5800x")
+    scouter1 = Scouter(RTX3070LINK, "3070", RTX3070XPATH)
+    scouter2 = Scouter(RYZEN7LINK, "5800x", RYZEN7XPATH)
     p1 = Process(target = scouter1.scout)
     p1.start()
     p2 = Process(target = scouter2.scout)
     p2.start()
+  
+    
+    
 
 
