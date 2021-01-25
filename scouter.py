4c57550564fd4a5a25b8d3bbda20a738df2ab48c
Edmund Wang <wangedmund@berkeley.edu>
2021-01-24 17:15:43 -0800

    added more requirements to requirements.txt

--- scouter.py
+++ scouter.py
@@ -1,6 +1,5 @@
 import requests
 from selenium import webdriver
-from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
 import os 
 from multiprocessing import Process
