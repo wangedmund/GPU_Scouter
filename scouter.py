9202c6219ca4622aa9149c60ccda41e291ba4e44
Edmund Wang <wangedmund@berkeley.edu>
2021-01-26 23:47:37 -0800

    kenji

--- scouter.py
+++ scouter.py
@@ -7,6 +7,11 @@ from webdriver_manager.chrome import ChromeDriverManager
 import time
 
 WEBHOOK = "https://discord.com/api/webhooks/758151139383443478/XURX5jr3xmCPN6OPoTV2bGP-YLhD8K_c4roR1_KivVZZ8TnPfOuuQqQVyrJdxQP1DV2A"
+UWUWEBHOOK = "https://discord.com/api/webhooks/803854081953103892/1KUvD1y7EJkoD2VUXr4TX5GxuOYLJMGkfeXWjK0lOgBlinlohxHP7Z1YlHbsHS_Cm1Et"
+RTX3060TILINK_NVIDIA = "https://www.bestbuy.com/site/nvidia-geforce-rtx-3060-ti-8gb-gddr6-pci-express-4-0-graphics-card-steel-and-black/6439402.p?skuId=6439402"
+RTX3060TIXPATH_NVIDIA = '//button[@data-sku-id = "6439402"]'
+RTX3060TILINK_GIGABYTE = "https://www.bestbuy.com/site/gigabyte-nvidia-geforce-rtx-3060-ti-eagle-oc-8g-gddr6-pci-express-4-0-graphics-card-black/6442485.p?skuId=6442485"
+RTX3060TIXPATH_GIGABYTE = '//button[@data-sku-id = "6442485"]'
 RTX3070LINK = "https://www.bestbuy.com/site/nvidia-geforce-rtx-3070-8gb-gddr6-pci-express-4-0-graphics-card-dark-platinum-and-black/6429442.p?skuId=6429442"
 RTX3070XPATH = '//button[@data-sku-id = "6429442"]'
 RTX3080LINK = "https://www.bestbuy.com/site/nvidia-geforce-rtx-3080-10gb-gddr6x-pci-express-4-0-graphics-card-titanium-and-black/6429440.p?skuId=6429440"
@@ -24,7 +29,7 @@ class Scouter():
         d = {
         "content": "<@&691835932835577856>\nBuy Now:\n" + link
         }
-        requests.post(WEBHOOK, json=d)
+        requests.post(UWUWEBHOOK, json=d)
     def scout(self):
         chrome_options = webdriver.ChromeOptions()
         chrome_options.binary_location = os.environ.get("GOOGLE_CHROME_BIN")
@@ -32,8 +37,8 @@ class Scouter():
         chrome_options.add_argument("--disable-dev-shm-usage")
         chrome_options.add_argument("--no-sandbox")
         # driver = webdriver.Chrome(executable_path=os.environ.get("CHROMEDRIVER_PATH"), chrome_options=chrome_options)
-        driver = webdriver.Chrome(ChromeDriverManager(version="87.0.4280.88").install())
-        # driver = webdriver.Chrome(LOCAL_PATH)
+        # driver = webdriver.Chrome(ChromeDriverManager(version="87.0.4280.88").install())
+        driver = webdriver.Chrome(LOCAL_PATH)
         driver.get(self.link)
         while True:
             try:
@@ -51,8 +56,8 @@ class Scouter():
             driver.refresh()
             
 if __name__=='__main__':
-    scouter1 = Scouter(RTX3070LINK, "3070", RTX3070XPATH)
-    scouter2 = Scouter(RYZEN7LINK, "5800x", RYZEN7XPATH)
+    scouter1 = Scouter(RTX3060TILINK_NVIDIA, "nvidia 3060ti", RTX3060TIXPATH_NVIDIA)
+    scouter2 = Scouter(RTX3060TILINK_GIGABYTE, "gigabyte 3060ti", RTX3060TIXPATH_GIGABYTE)
     p1 = Process(target = scouter1.scout)
     p1.start()
     p2 = Process(target = scouter2.scout)
