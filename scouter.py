01417b924fa43395c1173adea901b53ff897d5c0
Edmund Wang <wangedmund@berkeley.edu>
2021-06-09 19:39:30 -0700

    updated scouter

--- scouter.py
+++ scouter.py
@@ -19,6 +19,9 @@ RTX3060TIXPATH_GIGABYTE = '//button[@data-sku-id = "6442485"]'
 RTX3070LINK = "https://www.bestbuy.com/site/nvidia-geforce-rtx-3070-8gb-gddr6-pci-express-4-0-graphics-card-dark-platinum-and-black/6429442.p?skuId=6429442"
 RTX3070XPATH = '//button[@data-sku-id = "6429442"]'
 
+RTX3070TILINK = "https://www.bestbuy.com/site/nvidia-geforce-rtx-3070-ti-8gb-gddr6x-pci-express-4-0-graphics-card-dark-platinum-and-black/6465789.p?skuId=6465789"
+RTX3070TIXPATH = '//button[@data-sku-id = "6465789"]'
+
 RTX3080LINK = "https://www.bestbuy.com/site/nvidia-geforce-rtx-3080-10gb-gddr6x-pci-express-4-0-graphics-card-titanium-and-black/6429440.p?skuId=6429440"
 RTX3080XPATH = '//button[@data-sku-id = "6429440"]'
 
@@ -34,7 +37,8 @@ chrome_options.add_argument("--disable-dev-shm-usage")
 chrome_options.add_argument("--no-sandbox")
 chrome_options.add_argument("--example-flag")
 driver = webdriver.Chrome(ChromeDriverManager().install())
-# driver2 = webdriver.Chrome(ChromeDriverManager().install())
+driver2 = webdriver.Chrome(ChromeDriverManager().install())
+driver3 = webdriver.Chrome(ChromeDriverManager().install())
 class Scouter():
     def __init__(self, link, name, XPATH, driver):
         self.link = link
@@ -59,12 +63,10 @@ class Scouter():
                 print(self.name + " element could not be found!")
                 self.driver.refresh()
                 continue
-            if (elem.text == 'Sold Out'):
+            if (elem.text != 'Sold Out' and elem.text != 'Coming Soon'):
                 print(self.name + "'s " +  elem.text + " button found!")
-                duration = 300  # seconds
-                freq = 440  # Hz
-                os.system('play -nq -t alsa synth {} sine {}'.format(duration, freq))
-                return
+                for i in range(3):
+                    os.system('say "Graphics card potentially found"')
             else:
                 print(self.name + " " + elem.text)
             self.driver.refresh()
@@ -72,6 +74,10 @@ class Scouter():
 if __name__=='__main__':
     scouter1 = Scouter(RTX3070LINK, "nvidia 3070", RTX3070XPATH, driver)
     p1 = threading.Thread(target = scouter1.scout).start()
+    scouter2 = Scouter(RTX3080LINK, "nvidia 3080", RTX3080XPATH, driver2)
+    p2 = threading.Thread(target = scouter2.scout).start()
+    scouter3 = Scouter(RTX3070TILINK, "nvidia 3070ti", RTX3070TIXPATH, driver3)
+    p3 = threading.Thread(target = scouter3.scout).start()
    
   
     
